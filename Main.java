e7c7f050fec4a2f80801c4c64938caa0e4e40339
mkuo876@gmail.com <mkuo876@gmail.com>
2023-03-27 14:04:58 +0530

    completed stage 4

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public class Main {
     private static Scanner s = new Scanner(System.in);
     private static String secret = "";
     public static void main(String[] args) {
+        System.out.println("Please, enter the secret code's length:");
         int lengthOfSecretCode = s.nextInt();
         if (lengthOfSecretCode > 10) {
             System.out.println("Error: can't generate a secret number with a length of" +
@@ -19,6 +20,24 @@ public class Main {
                 secret = generateSecretCode(String.valueOf(System.nanoTime()), lengthOfSecretCode);
             }
             System.out.println(secret);
+            int[] result = new int[2];
+            int turn = 1;
+            System.out.println("Okay, let's start a game!");
+            while (result[1] != lengthOfSecretCode) {
+                result[0] = 0;
+                result[1] = 0;
+                System.out.printf("Turn %d:%n", turn);
+                String guessedInput = s.next();
+                result[0] = calculateCows(guessedInput, result[0]);
+                result[1] = calculateBulls(guessedInput, result, result[1]);
+                if (result[1] == lengthOfSecretCode) {
+                    System.out.printf("Grade: %d bulls%nCongratulations!" +
+                            " You guessed the secret code.",result[1]);
+                }
+                grade(result[0], result[1]);
+                turn++;
+            }
+
         }
     }
 
@@ -37,15 +56,27 @@ public class Main {
     }
 
     private static void grade(int cows, int bulls) {
-        if (bulls != 0)
-            System.out.printf("Grade:%d bull(s) %d cow(s). The secret code is %s",
-                    bulls, cows, secret);
-        else if (cows != 0)
-            System.out.printf("Grade: %d cow(s). The secret code is %s",
-                    cows, secret);
-        else
-            System.out.printf("Grade: None. The secret code is %s",
-                    secret);
+        if (bulls > 0 && cows > 0) {
+            if (bulls == 1 && cows == 1)
+                System.out.printf("Grade: %d bull and %d cow%n",bulls,cows);
+            System.out.printf("Grade: %d bulls and %d cows%n",bulls,cows);
+        } else if (bulls == 0 && cows != 0) {
+            if (cows == 1) {
+                System.out.printf("Grade: %d cow%n", cows);
+            }
+            else {
+                System.out.printf("Grade: %d cows%n", cows);
+            }
+        } else if (cows == 0 && bulls != 0) {
+            if (bulls == 1){
+                System.out.printf("Grade: %d bull%n", bulls);
+            }
+            else {
+                System.out.printf("Grade: %d bulls%n", bulls);
+            }
+        } else {
+            System.out.printf("Grade: None%n");
+        }
     }
     private static int calculateCows(String input, int cows) {
         char[] inputArray = input.toCharArray();
@@ -57,11 +88,11 @@ public class Main {
         }
         return cows;
     }
-    private static int calculateBulls(String input, int cows, int bulls) {
+    private static int calculateBulls(String input, int[] result, int bulls) {
         char[] inputArray = input.toCharArray();
         for (int i = 0; i < secret.length(); i++ ) {
             if (inputArray[i] == secret.charAt(i)) {
-                cows--;
+                result[0]--;
                 bulls++;
             }
         }
